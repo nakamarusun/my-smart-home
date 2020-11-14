1f00ff0f18b0b7444fb84ade71f24048b92a4584
Jason Christian H <nakamarujc@gmail.com>
2020-11-14 20:40:42 +0700

    Initialized Smart Doorbell Project

---
+++ src/Smart Doorbell/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <esp_camera.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
