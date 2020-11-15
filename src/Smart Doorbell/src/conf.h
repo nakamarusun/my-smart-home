25d3752650603aae38a23ec15601c44178849a1c
Jason Christian H <nakamarujc@gmail.com>
2020-11-15 12:54:03 +0700

    Succesfully tested the camera and created a server

---
+++ src/Smart Doorbell/src/conf.h
@@ -0,0 +1,14 @@
+/*
+Semua yang ada di file ini dapat diganti
+ke kemauan hatimu~
+*/
+
+// Detail AP
+#define AP_SSID "SMART_DOORBELL"
+#define AP_PASS "password"
+
+#define MDNS_NAME "smartdoor" // Nama mDNS yang bisa diakses dengan mudah
+
+#define AP_TIMEOUT 60 // Timeout dalam detik membuka mode AP
+
+#define CONNECT_TIMEOUT 30 // Timeout dalam detik untuk connect ke Wi-Fi
--- src/Smart Doorbell/src/main.cpp
+++ src/Smart Doorbell/src/main.cpp
@@ -1,10 +1,101 @@
 #include <Arduino.h>
-#include <esp_camera.h>
+#include <esp32cam.h>
+
+// Library ESP
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+// For Web server
+// Manager wifi connection
+#include <WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+
+#include "conf.h"
+
+// Variable server
+WebServer server(80);
+DNSServer dns;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // Mulai serial
+  Serial.begin(9600);
+
+  Serial.println("Initializing WiFi");
+
+  // WiFi.disconnect();
+
+  // Bikin WiFiManager baru
+  WiFiManager wifiManager;
+
+  // Disini kita bisa set timeout ketika ESP tidak di setting
+  // dalam jangka waktu yang ditentukan, bisa coba connect ke wi-fi lagi.
+  wifiManager.setConnectTimeout(CONNECT_TIMEOUT);
+  wifiManager.setConfigPortalTimeout(AP_TIMEOUT);
+  bool connected = false;
+
+  // Disini kita bisa menentukan apa nama dan password device kita
+  // untuk bisa membantu dia berhubung ke Wi-Fi yang kita inginkan
+  // Lakukan loop sampai koneksi dibuat.
+  do {
+
+    connected = wifiManager.autoConnect(AP_SSID, AP_PASS);
+    if (!connected) {
+      Serial.println("Gagal koneksi dari memory, membuka mode AP");
+      Serial.println("Belum terkoneksi... Mencoba lagi...");
+    }
+
+  } while (!connected);
+
+  Serial.println("Koneksi dibuat!");
+
+  // MDNS responder berguna supaya web servernya bisa langsung diakses
+  // dari MDNS_NAME.local, dan tidak usah memakai IPnya 
+  if (MDNS.begin(MDNS_NAME)) {
+    MDNS.addService("http", "tcp", 80);
+    Serial.print("mDNS responder dimulai di ");
+    Serial.println(MDNS_NAME);
+  } else {
+    Serial.print("mDNS responder gagal dimulai di ");
+    Serial.println(MDNS_NAME);
+  }
+
+  // Init kameranya dan beri konfigurasi yang sesuai.
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(Resolution::find(800, 600));
+    cfg.setBufferCount(3);
+    cfg.setJpeg(95);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "Kamera berhasil di aktifkan" : "Kamera gagal diaktifkan.");
+    Serial.println("Bruh?");
+  }
+
+  server.on("/", HTTP_GET, [](){
+    auto frame = esp32cam::capture();
+    if (frame == nullptr) {
+      Serial.println("CAPTURE FAIL");
+      server.send(503, "", "");
+      return;
+    }
+    Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+    server.setContentLength(frame->size());
+    server.send(200, "image/jpeg");
+    WiFiClient client = server.client();
+    frame->writeTo(client);
+  });
+
+  server.begin();
+  Serial.println("Semua sistem sukses di inisialisasi!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
 }
